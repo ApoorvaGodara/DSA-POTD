1a31d92f8e17f4f0e7a5b289af06252cebe4ab73
ApoorvaGodara <apoorvagodara29@gmail.com>
2025-10-22 10:20:16 +0530

    POTD 2025-10-22: #3347 Problem Name (LeetCode)

---
+++ leetcode/2025-10-22_3347Maxfreqafterop2.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int check(vector<int>& nums, int n, int t, int m) {
+        long long nL = n;
+        long long tL = t;
+        int l = lower_bound(nums.begin(), nums.end(), nL) - nums.begin();
+        int h = upper_bound(nums.begin(), nums.end(), nL) - nums.begin();
+        int ll = lower_bound(nums.begin(), nums.end(), nL - tL) - nums.begin();
+        int hh = upper_bound(nums.begin(), nums.end(), nL + tL) - nums.begin();
+        int res = (hh - h) + (l - ll);
+        return min(m, res) + (h - l);
+    }
+
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int m = numOperations;
+        sort(nums.begin(), nums.end());
+        int ans = 1;
+        for (int i = 0; i < (int)nums.size() - 1; i++) {
+            ans = max(check(nums, nums[i], k, m), ans);
+            ans = max(check(nums, nums[i] - k, k, m), ans);
+            ans = max(check(nums, nums[i] + k, k, m), ans);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n, k, numOperations;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter array elements: ";
+    for (int &x : nums) cin >> x;
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    cout << "Enter number of operations: ";
+    cin >> numOperations;
+
+    int result = sol.maxFrequency(nums, k, numOperations);
+    cout << "Maximum possible frequency: " << result << "\n";
+
+    return 0;
+}
